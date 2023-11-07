caa4f22e6f57e81a6d17e6d1c01ffaa5978baa4e
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-11-07 17:34:48 +0100

    [WIP] Plotting jacobian for sanity checks

--- tests/test_gradients/test_gradients_I.py
+++ tests/test_gradients/test_gradients_I.py
@@ -10,16 +10,19 @@ data_path = os.path.abspath('../../examples/')
 def test_gradients_I():
     geo_model: gp.data.GeoModel = gp.create_geomodel(
         project_name='Wells',
-        extent=[0, 12000, 0, 12000, 0, 4000],
-        refinement=6,  # * For this model is better not to use octrees because we want to see what is happening in the scalar fields
+        extent=[0, 12000, -500, 500, 0, 4000],
+        resolution=[20, 2, 20],
+        refinement=1,  # * For this model is better not to use octrees because we want to see what is happening in the scalar fields
         importer_helper=gp.data.ImporterHelper(
             path_to_orientations=data_path + "/data/2-layers/2-layers_orientations.csv",
             path_to_surface_points=data_path + "/data/2-layers/2-layers_surface_points.csv"
         )
     )
     
+    geo_model.interpolation_options.uni_degree = 0
     geo_model.interpolation_options.mesh_extraction = False
-    geo_model.interpolation_options.sigmoid_slope = 2000
+    geo_model.interpolation_options.sigmoid_slope = 1000.
+    
     gp.compute_model(
         gempy_model=geo_model,
         engine_config=gp.data.GemPyEngineConfig(
@@ -28,17 +31,33 @@ def test_gradients_I():
     )
 
     block = geo_model.solutions.octrees_output[0].last_output_center.final_block
-    sp_coords_tensor = geo_model.foo.surface_points.sp_coords
+    sp_coords_tensor = geo_model.taped_interpolation_input.surface_points.sp_coords
+    
     import torch
     jacobian = torch.zeros((
         sp_coords_tensor.shape[0],
         sp_coords_tensor.shape[1],
         block.shape[0])
     )
+
+    if False:
+        sp_coords_tensor.register_hook(lambda x: print("I am here!", x))
+
     for e, element in enumerate(block):
         element.backward(retain_graph=True)
         jacobian[:, :, e] = sp_coords_tensor.grad
-    
+
     print("Gradients:", jacobian)
-    
-    gpv.plot_2d(geo_model, show_topography=False, legend=False, show=True)
+
+    for i in range(4):
+        gradient_z_sp_1 = jacobian[i, 0, :].detach().numpy()
+        gpv.plot_2d(
+            geo_model,
+            show_topography=False,
+            legend=False,
+            show=True,
+            override_regular_grid=gradient_z_sp_1,
+            kwargs_lithology={
+                'cmap': 'viridis',
+            }
+        )
