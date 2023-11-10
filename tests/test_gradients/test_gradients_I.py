f7847a4f5974d87af8800dcdb87a2b799de6d2e0
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-11-10 15:03:39 +0100

    [WIP] Towards a more stable activation function... hopefully

--- tests/test_gradients/test_gradients_I.py
+++ tests/test_gradients/test_gradients_I.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 data_path = os.path.abspath('../../examples/')
 
 
-def model(sigoid_slope=100):
+def model(sigoid_slope=100, plot=False):
     geo_model: gp.data.GeoModel = gp.create_geomodel(
         project_name='Wells',
         extent=[0, 12000, -500, 500, 0, 4000],
-        resolution=[10, 1, 10],
+        resolution=np.array([10, 1, 10]) * 1,
         refinement=1,  # * For this model is better not to use octrees because we want to see what is happening in the scalar fields
         importer_helper=gp.data.ImporterHelper(
             path_to_orientations=data_path + "/data/2-layers/2-layers_orientations.csv",
@@ -28,23 +28,24 @@ def model(sigoid_slope=100):
             backend=gp.data.AvailableBackends.PYTORCH
         )
     )
+    
+    if plot:
+        gpv.plot_2d(geo_model, show_scalar=False, kwargs_lithology={
+            "plot_grid": True,
+            "cmap": "viridis"
+        })
+        plt.show()
     return geo_model
 
 
-def test_model():
-    geo_model=model(200)
-
-    gpv.plot_2d(geo_model, show_scalar=True, kwargs_lithology={"plot_grid": True})
-
-
 def test_gradients_numpy():
-    geo_model = model(1000)
+    geo_model = model(1000, plot=True)
 
     gpv.plot_2d(geo_model, kwargs_lithology={"plot_grid": True})
 
     par_val = geo_model.surface_points.data['Z'][0]
     var = 50
-    point_n = 1
+    point_n = 0
 
     values_to_compute = np.linspace(par_val - var, par_val + var, 30)
     arrays = np.array([])
@@ -95,6 +96,7 @@ def test_gradients_numpy():
         override_regular_grid=gradient_z_sp_1,
         kwargs_lithology={
             'cmap': 'seismic',
+            'norm': None,
             "plot_grid": True,
             "vmin": -max_abs_val,
             "vmax": max_abs_val
@@ -104,8 +106,7 @@ def test_gradients_numpy():
 
 
 def test_gradients_I():
-    geo_model = model(sigoid_slope=1200)
-
+    geo_model = model(sigoid_slope=1200, plot=True)
     # * This is the activated block
     block = geo_model.solutions.octrees_output[0].last_output_center.final_block
 
@@ -134,7 +135,7 @@ def test_gradients_I():
     print("Gradients:", jacobian)
     print("Max min:", jacobian.max(), jacobian.min())
 
-    for i in range(1, 2):
+    for i in range(0, 4):
         gradient_z_sp_1 = jacobian[i, 2, :].detach().numpy()
         
         max_abs_val = np.max(np.abs(gradient_z_sp_1))
@@ -146,6 +147,7 @@ def test_gradients_I():
             override_regular_grid=gradient_z_sp_1,
             kwargs_lithology={
                 'cmap': 'seismic',
+                "norm": None,
                 "plot_grid": True,
                 "vmin": -max_abs_val,
                 "vmax": max_abs_val
