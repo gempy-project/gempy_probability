48fc12094b8800d365f41182b7bf9c1440c7b534
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-09-05 12:31:59 +0200

    [DOC] Fixing minor stuff to be able to run sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,6 @@ import numpy as np
 pyvista.set_error_output_file('errors.txt')
 pyvista.OFF_SCREEN = True  # Not necessary - simply an insurance policy. Ensure that offscreen rendering is used for docs generation
 pyvista.set_plot_theme('document')  # Preferred plotting style for documentation
-pyvista.rcParams['window_size'] = np.array([1024, 768]) * 2
 pyvista.FIGURE_PATH = os.path.join(os.path.abspath('_images/'), 'auto-generated/')  # Save figures in specified directory
 pyvista.BUILDING_GALLERY = True
 if not os.path.exists(pyvista.FIGURE_PATH):
@@ -136,10 +135,10 @@ linkcheck_timeout = 500
 sphinx_gallery_conf = {
     # path to your examples scripts
     "examples_dirs"          : [
-        "../../examples/0-intro",
-        "../../examples/1-first_example_of_inference",
-        "../../examples/2-basic_geology",
-        "../../examples/3-probabilistic_inversion",
+        "../../examples/tutorials/0-intro",
+        "../../examples/tutorials/1-first_example_of_inference",
+        "../../examples/tutorials/2-basic_geology",
+        "../../examples/tutorials/3-probabilistic_inversion",
         "../../examples/utils",
     ],
     # path where to save gallery generated examples
