5d32fdaa50702a16410b70b6876e88ca4a7dcd25
MigueldelaVarga <miguel@terranigma-solutions.com>
2025-05-06 16:04:11 +0100

    [ENH][TEST] Add custom grid setups and profiling for performance
    Introduce custom grid configurations to improve control over model evaluations. Add profiling in test_benchmark to measure and analyze computation speeds and iterations. Update tests to track interpolation counts and backend changes.

--- tests/test_benchmark/test_speed_I.py
+++ tests/test_benchmark/test_speed_I.py
@@ -1,9 +1,14 @@
+import pytest
+
 import gempy as gp
 import gempy_viewer as gpv
+import numpy as np
+
 from gempy_probability.plugins.examples_generator import generate_example_model, ExampleProbModel
 
 
 def test_speed_I():
+    
     two_wells: gp.data.GeoModel = generate_example_model(
         example_model=ExampleProbModel.TWO_WELLS,
         compute_model=False
@@ -11,7 +16,34 @@ def test_speed_I():
 
     assert two_wells.interpolation_options.number_octree_levels == 4, "Number of octrees should be 4"
 
-    gp.compute_model(two_wells)
+    # region Minimal grid for the specific likelihood function
+    x_loc = 6000
+    y_loc = 0
+    z_loc = np.linspace(0, 4000, 100)
+    xyz_coord = np.array([[x_loc, y_loc, z] for z in z_loc])
+    gp.set_custom_grid(two_wells.grid, xyz_coord=xyz_coord)
+    # endregion
+
+    two_wells.grid.active_grids = gp.data.Grid.GridTypes.CUSTOM
+    
+    profiler = cProfile.Profile()
+    profiler.enable()
+    iterations = 100
+    for _ in range(iterations):
+        gp.compute_model(
+            gempy_model=two_wells,
+            engine_config=gp.data.GemPyEngineConfig(
+                backend=gp.data.AvailableBackends.numpy
+            )
+        )
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats('cumtime')
+    stats.print_stats(10)
 
     if PLOT := False:
         gpv.plot_2d(two_wells, show_scalar=False)
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_speed_on_gravity_likelihood():
+    pass
